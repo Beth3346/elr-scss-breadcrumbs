0368adce9452570c7a5f2ca877de530e61b7093f
Beth <beth3346@gmail.com>
2020-11-26 10:10:41 -0600

    update dependencies and readme

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # elr-scss-breadcrumbs
 
-[![Build Status](https://travis-ci.com/Beth3346/elr-scss-breadcrumbs.svg?branch=master)](https://travis-ci.com/Beth3346/elr-scss-breadcrumbs)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![npm](https://img.shields.io/npm/dm/elr-scss-breadcrumb.svg?style=flat)]()
 
@@ -11,7 +10,7 @@ a scss mixin for breadcrumbs
 Download node at [nodejs.org](http://nodejs.org) and install it, if you haven't already.
 
 ```sh
-npm install elr-scss-breadcrumbs --save
+npm install elr-scss-breadcrumbs
 yarn add elr-scss-breadcrumbs
 ```
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,68 @@
 const gulp = require('gulp');
 const path = require('path');
 const sass = require('gulp-sass');
-const scsslint = require('gulp-scss-lint');
+// const scsslint = require('gulp-scss-lint');
 const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const browsersync = require('browser-sync').create();
+const del = require('del');
 
 var paths = {
   app: 'dist/',
   css: 'dist/css/'
 };
 
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './dist/'
+    },
+    port: 3000
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean assets
+function clean() {
+  return del(['./dist/css/']);
+}
+
+// Watch files
+function watchFiles(done) {
+  gulp.watch(['./**/*.scss', 'dist/index.html'], () => {
+    return gulp
+      .src('./*.scss')
+      // .pipe(scsslint())
+      .pipe(plumber())
+      .pipe(
+        sass({
+          includePaths: ['node_modules']
+        }).on('error', sass.logError)
+      )
+      .pipe(gulp.dest(paths.css))
+      .pipe(
+        autoprefixer({
+          cascade: false
+        })
+      )
+      .pipe(gulp.dest(paths.css))
+      .pipe(cleanCSS({ debug: true }))
+      .pipe(gulp.dest(paths.css))
+      .pipe(browserSyncReload(done));
+  });
+}
+
 gulp.task('default', () => {
   return gulp
     .src('./*.scss')
-    .pipe(scsslint())
     .pipe(plumber())
     .pipe(
       sass({
@@ -24,7 +72,6 @@ gulp.task('default', () => {
     .pipe(gulp.dest(paths.css))
     .pipe(
       autoprefixer({
-        browsers: ['last 3 versions'],
         cascade: false
       })
     )
@@ -32,3 +79,8 @@ gulp.task('default', () => {
     .pipe(cleanCSS({ debug: true }))
     .pipe(gulp.dest(paths.css));
 });
+
+const watch = gulp.parallel(watchFiles, browserSync);
+
+exports.clean = clean;
+exports.watch = watch;
